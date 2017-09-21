fda83a542b9c9f84c0d1640c574fb404e3bedca0
DoriRunyon <dorileerunyon@gmail.com>
2017-09-20 21:16:58 -0700

    Added all scheduled requests

---
+++ all_users_off_watch.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from scheduled_requests import all_users_off_watch
+
+all_users_off_watch()
---
+++ third_request.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from scheduled_requests import subsequent_BAL_requests
+
+subsequent_BAL_requests()
