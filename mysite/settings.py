1bd3d0ba020620738452e9b8a3004867e90ca1a4
DoriRunyon <dorileerunyon@gmail.com>
2017-09-22 20:26:19 -0700

    Some style changes.

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,8 +81,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+#Database
+#https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
