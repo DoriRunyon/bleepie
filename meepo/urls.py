352bb85b3d6a52a03ac191635b38216d6f4a9e7d
DoriRunyon <dorileerunyon@gmail.com>
2017-10-10 11:13:38 -0400

    Updated about.html

--- meepo/urls.py
+++ meepo/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	url(r'^signup/$', views.signup, name='signup'),
+	url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url(r'^accounts/password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 	url(r'^$', views.about, name='about'),
 	url(r'^contact/$', views.contact, name='contact'),
     url(r'^pet_status', views.pet_status, name='pet_status'),
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', auth_views.login, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
-    url(r'^accounts/password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'', include('meepo.urls')),
 ]
